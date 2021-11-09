64d118b38a2678f2a1fdf4396e682ca0da35d9d0
feng <fenger>
2021-11-09 21:04:29 +0800

    update

---
+++ rsa基础类/P08 小e/test.py
@@ -0,0 +1,4 @@
+import gmpy2
+
+t, m = gmpy2.iroot(4, 2)
+print(t, m)
---
+++ rsa基础类/P18 m高位泄露/出题脚本.py
@@ -0,0 +1,18 @@
+import gmpy2
+import libnum
+import uuid
+
+flag = "flag{" + str(uuid.uuid4()) + "}"
+print(flag)
+m = libnum.s2n(flag)
+
+p = libnum.generate_prime(512)
+q = libnum.generate_prime(512)
+n = p * q
+m1 = ((m >> 12) << 12)
+e = 3
+c = pow(m, e, n)
+print("n=", n)
+print("c=", c)
+print("e=", e)
+print("m1=", m1)

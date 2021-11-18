fb02a3f49ce1b167e5b78d88dbf0b7f56953e5a7
feng <fenger>
2021-11-18 21:33:08 +0800

    update

--- rsa基础类/P19 p高位攻击/出题脚本.py
+++ rsa基础类/P19 p高位攻击/出题脚本.py
@@ -4,13 +4,16 @@ import uuid
 
 flag = "flag{" + str(uuid.uuid4()) + "}"
 m = libnum.s2n(flag)
-p = libnum.generate_prime(512)
-q = libnum.generate_prime(512)
+p = libnum.generate_prime(1024)
+q = libnum.generate_prime(1024)
 n = p * q
 e = 65537
-p1 = ((p >> 128) << 128)
+p1 = ((p >> 462) << 462)
+p2 = ((p >> 462))
 c = pow(m, e, n)
 print("n=", n)
 print("c=", c)
 print("e=", e)
-print("p1=", p)
+print("p =", p)
+print("p1=", p1)
+print("p2=", p2)

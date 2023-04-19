ca8c287a079a9e768fd13075407dc4310de7aadd
feng <fenger>
2023-04-19 20:36:19 +0800

    upload

--- RSA_基础篇/P21-P40/P33  pow(p, q, n) + pow(q, p, n)/test.py
+++ RSA_基础篇/P21-P40/P33  pow(p, q, n) + pow(q, p, n)/test.py
@@ -2,8 +2,8 @@ import libnum
 import uuid
 flag = "flag{" + str(uuid.uuid4()) + "}"
 m=libnum.s2n(flag)
-p=libnum.generate_prime(1024)
-q=libnum.generate_prime(1024)
+p=libnum.generate_prime(512)
+q=libnum.generate_prime(512)
 n=p*q
 print(p)
 print(q)
@@ -11,3 +11,6 @@ l1=pow(p,q,n)
 l2=pow(q,p,n)
 print(l1==p)
 print(l2==q)
+
+aa=pow(pow(p,n,n),1//p,n)
+print(aa)
